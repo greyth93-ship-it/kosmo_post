9a2d378c2b971686f0a599e7ebdad10d85ddda23
greyth93-ship-it <greyth93@gmail.com>
2026-04-16 18:07:43 +0900

    post3

--- src/test/java/com/grey/app/board/qna/QnaMapperTest.java
+++ src/test/java/com/grey/app/board/qna/QnaMapperTest.java
@@ -15,10 +15,31 @@ class QnaMapperTest {
 
 	@Autowired
 	private QnaMapper qnaMapper;
+	
 	@Test
-	void testList() throws Exception {
-		List<BoardDTO> ar = qnaMapper.list();
-		assertNotEquals(0, ar.size());
+	void testCreate() throws Exception{
+		for (int i=0;i<23;i++) {
+			QnaDTO qnaDTO = new QnaDTO();
+			qnaDTO.setBoardTitle("qtitle"+i);
+			qnaDTO.setBoardWriter("qwriter"+i);
+			qnaDTO.setBoardContent("qcontent"+i);
+			
+			qnaMapper.create(qnaDTO);
+			
+			if(i%3==0) {
+				Thread.sleep(500);
+			}
+			
+		}
+		System.out.println("finish");
+		
 	}
+	
+	
+//	@Test
+//	void testList() throws Exception {
+//		List<BoardDTO> ar = qnaMapper.list();
+//		assertNotEquals(0, ar.size());
+//	}
 
 }
